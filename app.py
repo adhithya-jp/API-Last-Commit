b1ac7b9ab6a330ce3188990c81b6d868cd189921
adhithya-jp <adhijp0007@gmail.com>
2026-04-22 11:39:56 +0530

    ready

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import re
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def solve():
+
+    if request.method == 'GET':
+        return "API is running"
+
+    data = request.get_json()
+    query = data.get("query", "")
+
+    nums = list(map(int, re.findall(r'\d+', query)))
+    total = sum(nums)
+
+    return jsonify({
+        "output": f"The sum is {total}."
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
